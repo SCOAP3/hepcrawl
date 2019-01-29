f02e330adb0f5dafe117b74101a0fa5e1780a501
Tamas Nyiri <tamas.nyiri@cern.ch>
2019-01-29 10:38:58 +0100

    aps: add tests

--- tests/test_aps.py
+++ tests/test_aps.py
@@ -11,6 +11,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import pytest
 
+from scrapy.http import TextResponse
 from hepcrawl.spiders import aps_spider
 from .responses import fake_response_from_file
 
@@ -18,24 +19,26 @@ from .responses import fake_response_from_file
 @pytest.fixture
 def results():
     """Return results generator from the WSP spider."""
-    from scrapy.http import TextResponse
 
-    spider = aps_spider.APSSpider()
-    records = list(
-        spider.parse(
-            fake_response_from_file(
-                'aps/aps_single_response.json',
-                response_type=TextResponse,
-            )
+    records = []
+
+    for file in ('aps/aps_single_response.json', 'aps/aps_single_response2.json'):
+        fake_response = fake_response_from_file(
+            file,
+            response_type=TextResponse,
         )
-    )
+        spider = aps_spider.APSSpider()
+        records.extend(list(spider.parse(fake_response)))
+
     assert records
+    assert len(records) == 2
+
     return records
 
 
 def test_abstract(results):
     """Test extracting abstract."""
-    abstract = (
+    abstracts = (
         "We use a popular fictional disease, zombies, in order to introduce techniques used in modern epidemiology"
         " modeling, and ideas and techniques used in the numerical study of critical phenomena. We consider variants of"
         " zombie models, from fully connected continuous time dynamics to a full scale exact stochastic dynamic"
@@ -43,134 +46,173 @@ def test_abstract(results):
         " analytical expression for the fully connected differential equation, and demonstrate that the single person"
         " per site two dimensional square lattice version of zombies lies in the percolation universality class. We end"
         " with a quantitative study of the full scale US outbreak, including the average susceptibility of different"
-        " geographical regions."
+        " geographical regions.",
+        ""
     )
-    for record in results:
-        assert 'abstract' in record
-        assert record['abstract'] == abstract
+    for abstract, record in zip(abstracts, results):
+        if abstract:
+            assert 'abstract' in record
+            assert record['abstract'] == abstract
+        else:
+            assert 'abstract' not in record
 
 
 def test_title(results):
     """Test extracting title."""
-    title = "You can run, you can hide: The epidemiology and statistical mechanics of zombies"
-    for record in results:
+    titles = ("You can run, you can hide: The epidemiology and statistical mechanics of zombies",
+              '')
+    for title, record in zip(titles, results):
         assert 'title' in record
         assert record['title'] == title
 
 
 def test_date_published(results):
     """Test extracting date_published."""
-    date_published = "2015-11-02"
-    for record in results:
+    dates_published = ("2015-11-02",
+                       '2015-11-02')
+    for date_published, record in zip(dates_published, results):
         assert 'date_published' in record
         assert record['date_published'] == date_published
 
 
 def test_page_nr(results):
     """Test extracting page_nr"""
-    page_nr = ["11"]
-    for record in results:
+    page_nrs = [["11"],
+                ['0']]
+    for page_nr, record in zip(page_nrs, results):
         assert 'page_nr' in record
         assert record['page_nr'] == page_nr
 
 
-def test_free_keywords(results):
-    """Test extracting free_keywords"""
-    pass
-
-
 def test_license(results):
     """Test extracting license information."""
-    expected_license = [{
-        'license': 'CC-BY-3.0',
-        'url': 'http://creativecommons.org/licenses/by/3.0/',
-    }]
-    for record in results:
+    expected_licenses = [
+        [{
+            'license': 'CC-BY-3.0',
+            'url': 'http://creativecommons.org/licenses/by/3.0/',
+        }],
+        [{
+            'license': 'CC-BY-3.0',
+            'url': 'http://creativecommons.org/licenses/by/3.0/',
+        }],
+    ]
+    for expected_license, record in zip(expected_licenses, results):
         assert 'license' in record
         assert record['license'] == expected_license
 
 
 def test_dois(results):
     """Test extracting dois."""
-    dois = "10.1103/PhysRevE.92.052801"
-    for record in results:
+    dois = ("10.1103/PhysRevE.92.052801",
+            "10.1103/PhysRevE.92.052801")
+    for doi, record in zip(dois, results):
         assert 'dois' in record
-        assert record['dois'][0]['value'] == dois
+        assert record['dois'][0]['value'] == doi
         break
 
 
 def test_collections(results):
     """Test extracting collections."""
-    collections = ['HEP', 'Citeable', 'Published']
-    for record in results:
+    collections = [['HEP', 'Citeable', 'Published'], ['HEP', 'Citeable', 'Published']]
+    for collection, record in zip(collections, results):
         assert 'collections' in record
-        for coll in collections:
+        for coll in collection:
             assert {"primary": coll} in record['collections']
 
 
 def test_collaborations(results):
     """Test extracting collaboration."""
-    collaborations = [{"value": "OSQAR Collaboration"}]
-    for record in results:
+    collaborations = [
+        [{"value": "OSQAR Collaboration"}],
+        [{"value": "OSQAR Collaboration"}]
+    ]
+    for collaboration, record in zip(collaborations, results):
         assert 'collaborations' in record
-        assert record['collaborations'] == collaborations
+        assert record['collaborations'] == collaboration
 
 
 def test_subjects(results):
     """Test extracting collaboration."""
-    subjects = [{
-        'scheme': 'APS',
-        'source': '',
-        'term': 'Quantum Information',
-    }]
-    for record in results:
+    subjects = [
+        [{
+            'scheme': 'APS',
+            'source': '',
+            'term': 'Quantum Information',
+        }],
+        [{
+            'scheme': 'APS',
+            'source': '',
+            'term': 'Quantum Information',
+        }]
+    ]
+    for subject, record in zip(subjects, results):
         assert 'field_categories' in record
-        assert record['field_categories'] == subjects
+        assert record['field_categories'] == subject
 
 
 def test_publication_info(results):
     """Test extracting dois."""
-    journal_title = "Phys. Rev. E"
-    journal_year = 2015
-    journal_volume = "92"
-    journal_issue = "5"
-    for record in results:
-        assert 'journal_title' in record
-        assert record['journal_title'] == journal_title
-        assert 'journal_year' in record
-        assert record['journal_year'] == journal_year
-        assert 'journal_volume' in record
-        assert record['journal_volume'] == journal_volume
-        assert 'journal_issue' in record
-        assert record['journal_issue'] == journal_issue
+    expected_results = (
+        dict(journal_title="Physical Review E",
+             journal_year=2015,
+             journal_volume="92",
+             journal_issue="5"),
+        dict(journal_title="Physical Review E",
+             journal_year=2015,
+             journal_volume="92",
+             journal_issue="5")
+    )
+    for expected, record in zip(expected_results, results):
+        for k, v in expected.items():
+            assert k in record
+            assert record[k] == v
 
 
 def test_authors(results):
     """Test authors."""
-    affiliation = 'Laboratory of Atomic and Solid State Physics, Cornell University, Ithaca, New York 14853, USA'
-    author_full_names = ['Alemi, Alexander A.', 'Bierbaum, Matthew', 'Myers, Christopher R.', 'Sethna, James P.']
-    for record in results:
+    expected_results = (
+        dict(
+            affiliation='Laboratory of Atomic and Solid State Physics, Cornell University, Ithaca, New York 14853, USA',
+            author_full_names=['Alemi, Alexander A.', 'Bierbaum, Matthew', 'Myers, Christopher R.', 'Sethna, James P.']
+        ),
+        dict(
+            affiliation='Laboratory of Atomic and Solid State Physics, Cornell University, Ithaca, New York 14853, USA',
+            author_full_names=['Alemi, Alexander A.', 'Bierbaum, Matthew', 'Myers, Christopher R.', 'Sethna, James P.']
+        )
+    )
+    for expected, record in zip(expected_results, results):
         assert 'authors' in record
-        assert len(record['authors']) == 4
+        assert len(record['authors']) == len(expected['author_full_names'])
 
         record_full_names = [author['full_name'] for author in record['authors']]
-        assert set(author_full_names) == set(record_full_names)  # assert that we have the same list of authors
+        assert set(expected['author_full_names']) == set(record_full_names)  # assert that we have the same list of authors
         for author in record['authors']:
-            assert author['affiliations'][0]['value'] == affiliation
+            assert author['affiliations'][0]['value'] == expected['affiliation']
 
 
 def test_copyrights(results):
     """Test extracting copyright."""
-    copyright_holder = "authors"
-    copyright_year = "2015"
-    copyright_statement = "Published by the American Physical Society"
-    copyright_material = "Article"
-    for record in results:
-        assert 'copyright_holder' in record
-        assert record['copyright_holder'] == copyright_holder
-        assert 'copyright_year' in record
-        assert record['copyright_year'] == copyright_year
-        assert 'copyright_statement' in record
-        assert record['copyright_statement'] == copyright_statement
-        assert 'copyright_material' in record
-        assert record['copyright_material'] == copyright_material
+    expected_results = (
+        dict(copyright_holder="authors",
+             copyright_year="2015",
+             copyright_statement="Published by the American Physical Society",
+             copyright_material="Article"),
+        dict(copyright_holder="authors",
+             copyright_year="2015",
+             copyright_statement="Published by the American Physical Society",
+             copyright_material="Article")
+    )
+    for expected, record in zip(expected_results, results):
+        for k, v in expected.items():
+            assert k in record
+            assert record[k] == v
+
+
+def test_report_numbers(results):
+    expected_results = (
+        [{'source': 'arXiv', 'value': 'arXiv:1806.06486'}],
+        [{'source': 'arXiv', 'value': ''}]
+    )
+
+    for expected, record in zip(expected_results, results):
+        assert record['report_numbers'] == expected
