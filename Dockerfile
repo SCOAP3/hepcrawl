26d1234e49e7b0f4bdad08f79897a8034e08fdae
Harris Tzovanakis <me@drjova.com>
2021-05-04 10:59:40 +0200

    docker: addition of a dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3
+FROM python:2.7
 
 WORKDIR /usr/src/app
 
@@ -7,7 +7,13 @@ RUN mkdir /code /var/lib/scrapy /venv
 
 copy . /code
 
-ENV PATH="/home/test/.local/bin:${PATH}"
-
 WORKDIR /code
-RUN pip install --no-cache-dir -e .
+
+RUN pip install --no-cache-dir --upgrade pip==20.3.4 && \
+    pip install --no-cache-dir --upgrade setuptools && \
+    pip install --no-cache-dir --upgrade wheel
+
+RUN pip install --no-cache-dir -r requirements.txt -e .
+
+RUN scrapyd  & \
+    sleep 10 && scrapyd-deploy
--- setup.py
+++ setup.py
@@ -13,42 +13,7 @@ from setuptools import setup, find_packages
 
 readme = open('README.rst').read()
 
-install_requires = [
-    'autosemver==0.5.2',
-    'jsonresolver==0.2.1',
-    'Twisted==17.5.0',
-    'Automat==0.6.0',
-    'dulwich==0.17.3',
-    'inspire-schemas==59.3.2',
-    'inspire-crawler==1.1.2',
-    'invenio-celery==1.0.0b2',
-    'invenio-pidstore==1.0.0',
-    'invenio-i18n==1.0.0',
-    'invenio-rest==1.0.0',
-    'invenio-files-rest==1.0.0b1',
-    'invenio-records-rest==1.4.2',
-    'invenio-records-files==1.0.0a11',
-    'invenio-accounts==1.0.2',
-    'pluggy==0.12.0',
-    'cookiecutter==1.4.0',
-    'invenio-base==1.0.2',
-    'Flask-Breadcrumbs==0.4.0',
-    'pyasn1-modules==0.0.9',
-    'Scrapy==1.4.0',
-    # TODO: unpin once they support wheel building again
-    'scrapyd==1.2.0',
-    'scrapyd-client==1.0.1',
-    'six==1.12.0',
-    'requests==2.20.0',
-    'celery==3.1.26.post2',
-    'redis==2.10.6',
-    'pyasn1==0.2.3',  # Needed for dependency resolving.
-    'LinkHeader==0.4.3',
-    'furl==0.5.6',
-    'ftputil==3.3.1',
-    'pysftp==0.2.9',
-    'python-dateutil==2.6.1',
-]
+install_requires = []
 
 tests_require = [
     'check-manifest==0.41',
@@ -75,6 +40,7 @@ extras_require = {
 
 setup_requires = [
     'pytest-runner~=2.7.0',
+    'autosemver==0.5.5',
 ]
 
 extras_require['all'] = []
