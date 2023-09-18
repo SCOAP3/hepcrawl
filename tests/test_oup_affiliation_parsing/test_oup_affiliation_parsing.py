57707d6f6b63d0018d8af4bd31cf6b934c52f944
ErnestaP <ernesta.petraityte@yahoo.com>
2023-09-18 13:07:08 +0200

    check

--- tests/test_oup_affiliation_parsing/test_oup_affiliation_parsing.py
+++ tests/test_oup_affiliation_parsing/test_oup_affiliation_parsing.py
@@ -7,8 +7,8 @@ from scrapy.selector import Selector
 
 correct_affiliations = {
     "2020_oup_ptaa186.xml": sorted([
-       "Institute of Science and Engineering, , Shimane University, , Matsue 690-8504, , Japan"
-    ]),
+        'Institute of Science and Engineering, , Shimane University, , Matsue 690-8504, , Japan',
+        'Department of Physical Sciences, College of Science and Engineering, , Ritsumeikan University, , Shiga 525-8577, , Japan']),
     "2021_oup_ptab168.xml": sorted([
         "Center for Gravitational Physics, Yukawa Institute for Theoretical Physics, Kyoto University, Kyoto 606-8502, Japan",
         "Theoretical Research Division, Nishina Center, RIKEN, Saitama 351-0198, JapanInterdisciplinary Theoretical and Mathematical Sciences Program (iTHEMS), RIKEN Saitama 351-0198, Japan",
@@ -33,14 +33,11 @@ def affiliations_from_records(shared_datadir):
 
 
 def test_country_in_OUP(affiliations_from_records):
-
+    print(affiliations_from_records, "HERE")
     for file_name in files_for_testing:
+        affiliations_values = []
         for affiliations_from_record in affiliations_from_records[file_name]:
-            affiliations_values = []
-            for affiliation_value_from_record in affiliations_from_record['affiliations']:
-                affiliations_values.append(
-                    affiliation_value_from_record['value'])
-            # checking, are values the same
             print(affiliations_values, correct_affiliations[file_name], file_name)
-            assert len(affiliations_values) == len(correct_affiliations[file_name])
-            assert sorted(affiliations_values) == correct_affiliations[file_name]
+            assert None
+            # assert len(affiliations_values) == len(correct_affiliations[file_name])
+            # assert sorted(affiliations_values) == correct_affiliations[file_name]
