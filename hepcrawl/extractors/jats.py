5975cdd030c1003ba91f3d46a13a53eaf9e4dd5c
ErnestaP <ernesta.petraityte@yahoo.com>
2023-09-18 15:42:00 +0200

    affiliations_values

--- hepcrawl/extractors/jats.py
+++ hepcrawl/extractors/jats.py
@@ -105,7 +105,7 @@ class Jats(object):
                     institution = get_first(aff.xpath("institution/text()").extract())
                     country_institution = {k: v for k, v in {'country': country, 'institution': institution}.items() if v is not None}
                     if 'country' in country_institution and 'institution' in country_institution:
-                        country_institution.append({'value':','.join([country_institution["institution"], country_institution["country"]])})
+                        affiliations_values.append({'value':','.join([country_institution["institution"], country_institution["country"]])})
                     affiliations_values.append({'value': self._clean_aff(aff)})
                 else:
                     # if aff. value captured by xpath is just new line, we have to take all data in aff tags.
